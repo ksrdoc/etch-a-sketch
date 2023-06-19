1af21154d3f816bf9b5a73799802b9c670a2e61f
ksrdoc <karlo.srdoc2@gmail.com>
2023-06-19 18:17:11 +0200

    made gridscale multiplayer variable

--- script.js
+++ script.js
@@ -7,5 +7,17 @@ function getSliderValue () {
 let sliderScale = document.getElementById ('sliderScale');
 
 slider.addEventListener ('input', function() {
+    let gridScale = slider.value * slider.value;
     sliderScale.innerText = slider.value + " " + "x" + " " + slider.value;
+    console.log ("Slider value:", slider.value)
+    console.log ("GridScale is:", gridScale)
 })
+
+
+
+
+
+
+
+
+
