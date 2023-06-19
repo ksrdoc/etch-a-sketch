db8b063f9217bc7d01a360545a65ff11e6e13154
ksrdoc <karlo.srdoc2@gmail.com>
2023-06-19 20:08:51 +0200

    Made a loop for chilld append into the grid. does not work

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Slider
+
 function getSliderValue () {
     const slider = document.getElementById('slider');
     const sliderValue = slider.value;
@@ -6,12 +8,28 @@ function getSliderValue () {
 
 let sliderScale = document.getElementById ('sliderScale');
 
+
+
+//Grid
+
+const grid = document.querySelector('.grid')
+
 slider.addEventListener ('input', function() {
     let gridScale = slider.value * slider.value;
     sliderScale.innerText = slider.value + " " + "x" + " " + slider.value;
     console.log ("Slider value:", slider.value)
     console.log ("GridScale is:", gridScale)
+
+    function makeCells (gridScale) {
+        for (let i = 0; i < gridScale; i++) {
+            const cell = document.createElement("cell");
+            cell.classList.add("cell");
+            grid.append(cell);
+        }
+    }
+    makeCells(gridScale);
 })
+
 
 
 
